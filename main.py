0c1b83e7df77a1ccdb59977c72ac6f1d2d4a1193
Carl Draper <draper.carl@gmail.com>
2022-02-18 19:18:59 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, make_response, Response, send_from_directory, send_file
+import time
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return Response("{'Data':'Hello World!'}", status=200, mimetype='application/json')
+
+@app.route("/delay")
+def delay():
+
+    delay = int(request.args.get("delay"))
+    time.sleep(delay)
+    return Response("{'Delay':{'" + str(delay) +"'}", status=200, mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', use_reloader=False)
